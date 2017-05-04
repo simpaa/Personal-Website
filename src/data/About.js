a852a292b5ccd9badf1097173b2be2a004e34b20
Simon Andersson <support@s-andersson.se>
2017-05-04 20:22:35 +0200

    Added data to about page.

---
+++ src/data/About.js
@@ -0,0 +1,6 @@
+export function getAboutContent() {
+  return {
+    'title': 'Hello!',
+    'text': 'My name is Simon Andersson i’m an 19 year old guy from Sweden. I have always liked to be creative. I’m doing iOS app development, web development and sometimes android app development. When I’m not programming I enjoy mountain biking and extreme unicycling.'
+  }
+}
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
+import { getAboutContent } from './../../data/About';
 import TextCard from './../../components/TextCard';
+
+var AboutContent = getAboutContent();
 
 class About extends Component {
   render() {
     return (
       <div>
-        <TextCard title="Test" text="test test"/>
+        <TextCard title={AboutContent.title} text={AboutContent.text}/>
       </div>
     );
   }
