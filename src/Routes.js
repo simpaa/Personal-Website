b00be5e92f50bab87f459300f8e78ac8ab5a7458
Simon Andersson <support@s-andersson.se>
2017-05-05 21:02:21 +0200

    Fixed footer at bottom.

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './pages/About';
