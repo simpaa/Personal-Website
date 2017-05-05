d232bbb6431f6a89b6d87c6dbb6114ca2b9829a9
Simon Andersson <support@s-andersson.se>
2017-05-05 10:22:40 +0200

    Added link to footer.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,9 @@ class Footer extends Component {
   render() {
     return (
       <footer className="main-footer">
-        <p>&copy; Simon Andersson 2017. See the source of this web page on <a href="#">GitHub</a>.</p>
+        <p>
+          &copy; Simon Andersson 2017. See the source of this web page on <a href="https://github.com/simpaa/Personal-Website" target="_blank">GitHub</a>.
+        </p>
       </footer>
     );
   }
