9ec27601e2262cc1cdb8e33a6b240b14f2ebc164
Simon Andersson <support@s-andersson.se>
2017-05-05 21:05:03 +0200

    Added text col inside item.

--- src/components/PortfolioList/ListItem.js
+++ src/components/PortfolioList/ListItem.js
@@ -5,11 +5,14 @@ class ListItem extends Component {
   render() {
 
     let description = this.props.object.description
+    let descriptionExcerpt = description.substr(0, EXCERPT_LENGHT) + '...'
 
     return (
       <li className="portfolio-item">
-        <h1>{this.props.object.title}</h1>
-        <p>{description.substr(0, EXCERPT_LENGHT) + '...'}</p>
+        <div className="portfolio-text-col">
+          <h1>{this.props.object.title}</h1>
+          <p>{descriptionExcerpt}</p>
+        </div>
       </li>
     );
   }
