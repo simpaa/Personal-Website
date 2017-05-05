e68d202ade7a856bd7b29cfe58f77a4299065382
Simon Andersson <support@s-andersson.se>
2017-05-05 23:58:07 +0200

    Added social media links.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 
 import './footer.css';
+import './../../font-awesome/css/font-awesome.min.css';
 
 class Footer extends Component {
   render() {
     return (
       <footer className="main-footer">
+
         <p>
           &copy; Simon Andersson 2017. See the source of this web page on <a href="https://github.com/simpaa/Personal-Website" target="_blank">GitHub</a>.
         </p>
+
+        <ul className="social-media">
+          <li><a href="https://twitter.com/simpa__" target="_blank"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
+          <li><a href="https://www.instagram.com/s_andersson_/" target="_blank"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
+          <li><a href="https://github.com/simpaa" target="_blank"><i className="fa fa-github" aria-hidden="true"></i></a></li>
+        </ul>
+
       </footer>
     );
   }
