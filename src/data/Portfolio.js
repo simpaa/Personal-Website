cfd80a2d05d0823cb458a83a509b8c798ca0bcc4
Simon Andersson <support@s-andersson.se>
2017-05-06 20:20:18 +0200

    Added real portfolio data.

--- src/data/Portfolio.js
+++ src/data/Portfolio.js
@@ -1,16 +1,16 @@
 export function getPortfolioData() {
   return [
     {
-      title: "test1",
-      description: "test",
+      title: "MX Service Journal",
+      description: "MXSJournal is a digital service journal for cross and enduro motorcycle riders that i made. It is an app available for both iOS (iPhone, iPad) and Android. I also have a website for the app in marketing purpose.",
       type: "website",
-      link: "#"
+      link: "http://mxsjournal.s-andersson.se/"
     },
     {
-      title: "test2",
-      description: "test test",
+      title: "No Candy",
+      description: "My friend come with the idea behind this app where you can keep track of how long you have avoid eating candy. I saw this idea good enough to practice on and release on the app store to get a feel for the process.",
       type: "app",
-      link: "#"
+      link: "https://itunes.apple.com/us/app/no-candy/id1171759085?mt=8"
     }
   ]
 }
