cf1e9c4fc0696057a3e8107d3646df42ad072ee5
Simon Andersson <support@s-andersson.se>
2017-05-06 20:44:16 +0200

    Revert "Changed let to const."
    This reverts commit 37d41d73d25211481004c4b9d46a6bda0f73b2c3.

--- src/components/PortfolioList/ListItem.js
+++ src/components/PortfolioList/ListItem.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
 import { EXCERPT_LENGHT } from './../../Constants';
-import LinkButton from './../LinkButton';
 
 class ListItem extends Component {
   render() {
 
-    const description = this.props.object.description;
-    const descriptionExcerpt = description.substr(0, EXCERPT_LENGHT) + '...';
-    const itemLink = `/portfolio/${this.props.id}`;
+    let description = this.props.object.description
+    let descriptionExcerpt = description.substr(0, EXCERPT_LENGHT) + '...'
 
     return (
       <li className="portfolio-item">
-        <Link to={itemLink}>
+        <Link to={`/portfolio/${this.props.id}`}>
           <div className="portfolio-text-col">
             <h1>{this.props.object.title}</h1>
             <p>{descriptionExcerpt}</p>
