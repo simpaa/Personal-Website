2bc6304dc97b8db04b449c6db5c75e893110da44
Simon Andersson <support@s-andersson.se>
2017-05-06 20:46:11 +0200

    Changed let to const.

--- src/components/PortfolioList/ListItem.js
+++ src/components/PortfolioList/ListItem.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+
 import { EXCERPT_LENGHT } from './../../Constants';
 
 class ListItem extends Component {
   render() {
-
-    let description = this.props.object.description
-    let descriptionExcerpt = description.substr(0, EXCERPT_LENGHT) + '...'
+    const description = this.props.object.description
+    const descriptionExcerpt = description.substr(0, EXCERPT_LENGHT) + '...'
+    const itemLink = `/portfolio/${this.props.id}`;
 
     return (
       <li className="portfolio-item">
-        <Link to={`/portfolio/${this.props.id}`}>
+        <Link to={itemLink}>
           <div className="portfolio-text-col">
             <h1>{this.props.object.title}</h1>
             <p>{descriptionExcerpt}</p>
