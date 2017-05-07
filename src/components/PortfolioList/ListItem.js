244ee5b1172d8bc13a8d8b67349e38c4dddffe0a
Simon Andersson <support@s-andersson.se>
2017-05-07 11:50:32 +0200

    Cleaned up.

--- src/components/PortfolioList/ListItem.js
+++ src/components/PortfolioList/ListItem.js
@@ -5,15 +5,15 @@ import { EXCERPT_LENGHT } from './../../Constants';
 
 class ListItem extends Component {
   render() {
-    const description = this.props.object.description;
-    const descriptionExcerpt = description.substr(0, EXCERPT_LENGHT) + '...';
+    const object = this.props.object;
+    const descriptionExcerpt = object.description.substr(0, EXCERPT_LENGHT) + '...';
     const itemLink = `/portfolio/${this.props.id}`;
 
     return (
       <li className="portfolio-item">
         <Link to={itemLink}>
           <div className="portfolio-text-col">
-            <h1>{this.props.object.title}</h1>
+            <h1>{object.title}</h1>
             <p>{descriptionExcerpt}</p>
           </div>
         </Link>
