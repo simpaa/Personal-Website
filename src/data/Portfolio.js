705485ef405fe36eb5b56e96ede5740f6c73ca3e
Simon Andersson <support@s-andersson.se>
2017-05-08 16:36:46 +0200

    Changed image.

--- src/data/Portfolio.js
+++ src/data/Portfolio.js
@@ -3,7 +3,7 @@ export function getPortfolioData() {
     {
       title: "MX Service Journal",
       description: "MXSJournal is a digital service journal for cross and enduro motorcycle riders that i made. It is an app available for both iOS (iPhone, iPad) and Android. I also have a website for the app in marketing purpose.",
-      image: "https://s-andersson.se/images/mxsjournal-website.png",
+      image: "https://s-andersson.se/images/mxsjournal_icon.png",
       skills: ["Swift", "Xcode", "Java", "Android Studio", "Wordpress theme development", "php", "jQuery", "scss"],
       type: "website",
       link: "http://mxsjournal.s-andersson.se/"
